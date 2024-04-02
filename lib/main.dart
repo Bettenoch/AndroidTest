a0afaffaba6704a209f6f0b56ed0a2f2d3915fb0
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2024-04-02 17:03:50 +0300

    Create separate app component

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,12 @@
-import 'package:adroidprojects/utils/theme/theme.dart';
+
+import 'package:adroidprojects/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-
-    );
-  }
-}
 
 
