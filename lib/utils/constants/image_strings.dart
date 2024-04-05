64d0fbccd537d517b182ae491e6cead6b32d4e9d
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2024-04-05 09:28:49 +0300

    Add gifs to assets.

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -7,4 +7,9 @@ class TImages {
   static const String google = "assets/logos/google.png";
   static const String facebook = "assets/logos/facebook.png";
   static const String twitter = "assets/logos/twitter.png";
+
+  static const String onBoardingImage1 = "assets/images/anime1.gif";
+  static const String onBoardingImage2 = "assets/images/anime3.gif";
+  static const String onBoardingImage3 = "assets/images/delivery.gif";
+  static const String onBoardingImage4 = "assets/images/delivery2.gif";
 }
