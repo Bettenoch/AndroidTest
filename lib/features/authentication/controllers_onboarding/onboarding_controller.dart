81a1b76be6cee0aa5d5e61de5fa58ba08c7745f2
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2024-04-05 13:35:36 +0300

    Create onboarding controller.

--- lib/features/authentication/controllers_onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers_onboarding/onboarding_controller.dart
@@ -1,13 +1,31 @@
+import 'package:adroidprojects/features/authentication/screens/login/login.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class OnBoardingController extends GetxController{
   static OnBoardingController get instance => Get.find();
+  //variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+  void updatePageIndicator(index) => currentPageIndex.value = index;
 
-  void updatePageIndicator(index) {}
 
-  void dotNavigationClick(index) {}
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index);
+  }
 
-  void nextPage() {}
+  void nextPage() {
+    if(currentPageIndex.value == 2){
+      Get.to(const LoginScreen());
+    }else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
 
-  void skipPage() {}
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
 }
