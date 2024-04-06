214278abe68f4414f116ef6b1c042a72df90ca86
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2024-04-06 16:56:38 +0300

    Add link to sign up page

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,4 +1,6 @@
+import 'package:adroidprojects/features/authentication/screens/signup/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../../utils/constants/sizes.dart';
@@ -45,7 +47,7 @@ class TLoginForm extends StatelessWidget {
             const SizedBox(height: TSizes.spaceBetweenSections),
             SizedBox(width: double.infinity, child: ElevatedButton(onPressed: (){}, child: const Text('Sign In'))),
             const SizedBox(height: TSizes.spaceBetweenItems),
-            SizedBox(width: double.infinity, child: OutlinedButton(onPressed: (){}, child: const Text('Create Account'))),
+            SizedBox(width: double.infinity, child: OutlinedButton(onPressed: ()=> Get.to(() => const SignupScreen()), child: const Text('Create Account'))),
 
           ],
 
