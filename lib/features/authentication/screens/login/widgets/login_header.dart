8032feb6b4de2c2a55471e4469589ce36c4f2ba2
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2024-04-06 14:36:58 +0300

    Create login header widget.

---
+++ lib/features/authentication/screens/login/widgets/login_header.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/constants/image_strings.dart';
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/helpers/helper_functions.dart';
+
+class TLoginHeader extends StatelessWidget {
+  const TLoginHeader({
+    super.key,
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image(
+            height: 150,
+            image: AssetImage( dark? TImages.darkAppLogo: TImages.darkAppLogo)
+        ),
+        Text('Login into your account', style: Theme.of(context).textTheme.headlineMedium),
+        const SizedBox(height: TSizes.sm),
+        Text('Choose your preferred login', style: Theme.of(context).textTheme.bodyMedium),
+      ],
+    );
+  }
+}
