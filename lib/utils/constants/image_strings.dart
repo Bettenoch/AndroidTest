0462d745d4e633868dd3684a60c13d223ff95827
Bettenoch <99537435+Bettenoch@users.noreply.github.com>
2024-04-15 16:18:18 +0300

    Add promo image strings.

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -12,4 +12,21 @@ class TImages {
   static const String onBoardingImage2 = "assets/images/anime3.gif";
   static const String onBoardingImage3 = "assets/images/delivery.gif";
   static const String onBoardingImage4 = "assets/images/delivery2.gif";
+
+  //random images
+
+  static const String icon1 = "assets/icons/alarm-clock.png";
+  static const String icon2 = "assets/icons/binder-clip.png";
+  static const String icon3 = "assets/icons/book.png";
+  static const String icon4 = "assets/icons/calendar.png";
+  static const String icon5 = "assets/icons/laptop-computer.png";
+  static const String icon6 = "assets/icons/laptop-screen.png";
+  static const String icon7 = "assets/icons/trophy.png";
+  static const String icon8 = "assets/icons/web-design.png";
+
+  //promotions
+  static const String promoIcon1 = "assets/promos/promopic1.png";
+  static const String promoIcon2 = "assets/promos/promopic2.png";
+  static const String promoIcon3 = "assets/promos/promopic3.png";
+  static const String promoIcon4 = "assets/promos/promopic4.png";
 }
